7a440a975899afbdefcaa20066220ee493d34989
Sally Li <sally.xinli@gmail.com>
2020-03-02 23:32:43 -0500

    added JPA entity type

---
+++ src/main/java/com/example/restservice/model/CustomerCreditInfo.java
@@ -0,0 +1,50 @@
+package com.example.restservice.model;
+
+import java.util.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.Type;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "CustomerCredit")
+public class CustomerCreditInfo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  @Column(name="uuid")
+  private final UUID uuid = UUID.randomUUID();
+  @Column(name="name")
+  private String name;
+  @Column(name="ssn")
+  private int ssn;
+  @Type(type = "com.example.mytastyserver.util.GenericArrayUserType")
+  @Column(
+    name = "tags",
+    columnDefinition = "int[]"
+  )
+  private int[] tags;
+
+//   private List<Integer> tags; 
+
+    public CustomerCreditInfo (String name, int ssn, int[] tags) {
+        this.name = name;
+        this.ssn = ssn;
+        this.tags = tags;
+    }
+
+    public long getId(){ return id; }
+    public void setId(long id) { this.id = id;}
+    public String getName() { return name;}
+    public void setName(String name) { this.name = name; }
+    public int getSSN() { return ssn; }
+    public void setSSN(int ssn) { this.ssn = ssn;}
+    public int[] getTags(){return this.tags;}
+    public void setTags(int[] tags) {this.tags = tags;}
+}
+
