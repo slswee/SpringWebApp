06981d02e01ed97120956f79b62bbc5e879b12e4
Sally Li <sally.xinli@gmail.com>
2020-03-08 19:47:10 -0400

    fixed unit test

--- src/main/java/com/example/restservice/controller/CustomerCreditInfoController.java
+++ src/main/java/com/example/restservice/controller/CustomerCreditInfoController.java
@@ -41,15 +41,14 @@ public class CustomerCreditInfoController {
 
     List<CustomerCreditInfo> customers;
     try {
-      customers = readFile("test.dat");
-      // customerCreditInfoService.saveCustomerCreditInfo(customers);
+      customers = getCustomerFromFileAndSave("test.dat");
 
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
-  private List<CustomerCreditInfo> readFile(String filename) throws IOException {
+  private List<CustomerCreditInfo> getCustomerFromFileAndSave(String filename) throws IOException {
     List<CustomerCreditInfo> customerList = new ArrayList<>();
 
     FileInputStream inputStream = null;
@@ -97,36 +96,6 @@ public class CustomerCreditInfoController {
             sc.close();
         }
     }
-
-
-      // try {
-      //       Stream<String> lines = Files.lines(Paths.get(filename)).skip(1).limit(10);
-
-      //         customerList = lines.map(currentLine -> {
-      //           try {
-      //             StringBuilder sb = new StringBuilder(currentLine);
-      //             String name = sb.substring(0, 72).trim();
-      //             int ssn = Integer.valueOf(sb.substring(72, 81));
-      //             List<Integer> tags = new ArrayList<>();
-      //             for (int i = 81; i < sb.length(); i += 9) {
-      //               // we get rid of the space 
-      //               int tag = Integer.valueOf(sb.substring(i, i + 9).trim());
-      //               // we are adding the negative numbers to the tags
-      //               tags.add(tag);
-      //             }
-      //             return new CustomerCreditInfo(name, ssn, tags);
-      //           } catch(Exception e) {
-      //             throw e;
-      //           }
-      //         })
-      //         .collect(Collectors.toList());
-      //     }
-      //     catch (FileNotFoundException e) {
-      //         System.err.println("input file not found");
-      //     }
-      //     catch (IOException e) {
-      //         System.err.println("Error reading file");
-      //     }
           return customerList;
       }
 }
--- src/test/java/com/example/restservice/controller/CustomerCreditInfoControllerTest.java
+++ src/test/java/com/example/restservice/controller/CustomerCreditInfoControllerTest.java
@@ -6,42 +6,51 @@ import com.example.restservice.service.CustomerCreditInfoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.stereotype.Service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
 @ExtendWith(MockitoExtension.class)
 public class CustomerCreditInfoControllerTest {
+    @InjectMocks
     private CustomerCreditInfoController controller;
-
     @Mock
     private CustomerCreditInfoService service;
 
-    @BeforeEach
-    void init() {
-        controller = new CustomerCreditInfoController();
-    }
+    private final String testID = "b8866061-2380-435d-b506-d0ae4ff6cf31";
+
     @Test
  public void getCustomerCreditInfo() {
   CustomerCreditInfo mockCustomer = new CustomerCreditInfo();
-  String testID = "b8866061-2380-435d-b506-d0ae4ff6cf31";
+
   UUID testUUID = UUID.fromString(testID);
-  when(service.findCustomerCreditInfo(testUUID)).thenReturn(mockCustomer);
+  when(service.findCustomerCreditInfo(any())).thenReturn(mockCustomer);
     CustomerCreditInfo response = controller.CustomerCreditInfo(testID);
 
     assertThat(response).isSameAs(mockCustomer);
     verify(service).findCustomerCreditInfo(testUUID);
+    
+  }
 
+  @Test
+  public void getTags() {
+    List<Integer> tags = Arrays.asList(583047,460668,518620,907327,-1,28253);
+    CustomerCreditInfo mockCustomer = new CustomerCreditInfo("coolName", 1234, tags);
+    when(service.findCustomerCreditInfo(any())).thenReturn(mockCustomer);
+    List<Integer> response = controller.CustomerTags(testID);
+
+    assertThat(response).isSameAs(tags);
     
   }
   
